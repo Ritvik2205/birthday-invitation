18d1c89f959817ccd864730ec77d64778290afdf
Ritvik <ritvik.gupta2205@gmail.com>
2024-08-03 03:05:50 +0530

    fading in and out

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#click-me').addEventListener('click', function() {        
         // document.querySelector('.card').style.display = 'none';
-        document.querySelector('svg').style.display = 'none';
-        document.querySelector('.card').style.display = 'block';
+        // document.querySelector('svg').style.display = 'none';
+        // document.querySelector('.card').style.display = 'block';
+        $('svg').fadeOut('slow', function() {
+            $('.card').fadeIn('slow');
+        });
     })
 
     
